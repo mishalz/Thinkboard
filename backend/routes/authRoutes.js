e85a6afa0bc25ed7fde160a27d9a2d37d552aef2
mishalz <mishal.zulfiqar2911@gmail.com>
2025-06-27 16:05:51 +0100

    added auth routes, auth middleware and auth controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,3 +1,17 @@
-const authRoutes = () => {};
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/authController");
 
-module.exports = authRoutes;
+const router = express.Router();
+
+//Auth Routes
+router.post("/register", registerUser); // register user
+router.post("/login", loginUser); // login user
+router.get("/profile", protect, getUserProfile); // Get User profile
+router.put("/profile", protect, updateUserProfile); // update profile
+
+module.exports = router;
